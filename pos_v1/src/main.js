dafa50aa6d9da840647905e55a2d0439e80e56ba
liu102401 <441606990@qq.com>
2014-12-03 22:28:47 +0800

    get_barcode

--- pos_v1/src/main.js
+++ pos_v1/src/main.js
@@ -1,1 +1,39 @@
 //TODO: Please write code in this file.
+
+function get_barcode_from_barcode_information(input_barcode_information){
+    var index_of_separator = input_barcode_information.indexOf('-');
+    if(index_of_separator == -1)
+        return input_barcode_information;
+    return input_barcode_information.substring(0, index_of_separator);
+}
+
+function get_number_from_barcode_information(input_barcode_information){
+
+}
+
+function compute_shopping_list(inputs){
+    var shopping_list = [];
+    var barcode;
+    for(var i = 0; i < inputs.length; i++){
+        barcode = get_barcode(inputs[i]);
+    }
+}
+
+function print_shopping_list(){
+
+}
+
+function print_free_goods_list(){
+
+}
+
+function print_summary_information(){
+
+}
+
+function printInventory(inputs){
+    var all_items = loadAllItems();
+    var promotions_informations = loadPromotions();
+    compute_shopping_list(inputs);
+}
+
