50a5993ec77675482927c714261012d5a65aecef
liu102401 <441606990@qq.com>
2014-12-03 23:10:53 +0800

    get_shopping_list

--- pos_v1/src/main.js
+++ pos_v1/src/main.js
@@ -8,15 +8,31 @@ function get_barcode_from_barcode_information(input_barcode_information){
 }
 
 function get_number_from_barcode_information(input_barcode_information){
+    var index_of_separator = input_barcode_information.indexOf('-');
+    if(index_of_separator == -1)
+        return 1;
+    return parseInt(input_barcode_information.substr(index_of_separator + 1));
+}
+
+function add_goods_information_to_shopping_list(shopping_list, barcode, number){
+    if(shopping_list[barcode] == null){
+        shopping_list[barcode] = number;
+        return;
+    }
+    shopping_list[barcode] += number
 
 }
 
-function compute_shopping_list(inputs){
+function get_shopping_list(inputs){
     var shopping_list = [];
     var barcode;
+    var number;
     for(var i = 0; i < inputs.length; i++){
-        barcode = get_barcode(inputs[i]);
+        barcode = get_barcode_from_barcode_information(inputs[i]);
+        number = get_number_from_barcode_information(inputs[i]);
+        add_goods_information_to_shopping_list(shopping_list, barcode, number);
     }
+    return shopping_list;
 }
 
 function print_shopping_list(){
@@ -32,8 +48,10 @@ function print_summary_information(){
 }
 
 function printInventory(inputs){
+    var shopping_list;
     var all_items = loadAllItems();
     var promotions_informations = loadPromotions();
-    compute_shopping_list(inputs);
+    shopping_list = get_shopping_list(inputs);
+    print_shopping_list();
 }
 
